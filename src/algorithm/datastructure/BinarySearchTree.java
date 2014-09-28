bfcc1d4c59f3f7a2aa24c3cc68d02928d1423d71
Guannan <gzhang428@gmail.com>
2014-09-28 08:02:20 -0700

    Complete binary tree

--- src/algorithm/datastructure/BinarySearchTree.java
+++ src/algorithm/datastructure/BinarySearchTree.java
@@ -13,36 +13,27 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	boolean isEmpty(){
 		return root == null;
 	}
-	
 
 	public void insert(T t){
-		TreeNode<T> newNode = new TreeNode<>(t);
-		if (isEmpty()){
-			root = newNode;
-			return;
-		}
-		
-		TreeNode<T> p = null;
-		TreeNode<T> node = root;
-		while(node != null){ 
-			p = node;
-			if (t.compareTo(node.key) <= 0){
-				node = node.left;
-			} else if (t.compareTo(node.key) > 0){
-				node = node.right;
-			} 
-		}
-		if (t.compareTo(p.key) <= 0){
-			p.left = newNode;
-		} else {
-			p.right = newNode;
-		}
+		root = insert(root, t);
 	}
 	
-	
-	
-	public void deleteRecursive(T t){
-		root =  delete(root, t);
+	private TreeNode<T> insert(TreeNode<T> node, T t) {
+		if (node == null){
+			return new TreeNode<T>(t);
+		}
+		
+		if (t.compareTo(node.key) <= 0){
+			node.left = insert(node.left, t);
+		} else {
+			node.right = insert(node.right, t);
+		}
+		node.size ++;
+		return node;
+	}
+
+	public void delete(T t){
+		root = delete(root, t);
 	}
 	
 	private TreeNode<T> delete(TreeNode<T> node, T t) {
@@ -56,10 +47,11 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			node.right = delete(node.right, t);
 		} else {
 			if (node.left != null && node.right != null){
-				TreeNode<T> replaceNode = max(node.left);
+				TreeNode<T> replaceNode = maxSubtreeNode(node.left);
 				node.left = delete(node.left, replaceNode.key);
 				replaceNode.left = node.left;
 				replaceNode.right = node.right;
+				replaceNode.size = node.left.size + node.right.size + 1;
 				return replaceNode;
 			} else if (node.left != null && node.right == null){
 				return node.left;
@@ -69,61 +61,72 @@ public class BinarySearchTree<T extends Comparable<T>> {
 				return null;
 			}
 		}
+		node.size --;
 		return node;
 	}
 
 	public boolean search(T t){
-		TreeNode<T> node = root;
-		while(node != null){
-			if (t.compareTo(node.key) == 0){
-				return true;
-			} else if (t.compareTo(node.key) < 0){
-				node = node.left;
-			} else {
-				node = node.right;
-			}
-		}
-		return false;
+		return search(root, t);
 	}
 	
+	private boolean search(TreeNode<T> node, T t) {
+		if (node == null){
+			return false;
+		}
+		
+		if (t.compareTo(node.key) == 0){
+			return true;
+		} else if (t.compareTo(node.key) < 0){
+			return search(node.left, t);
+		} else {
+			return search(node.right, t);
+		}
+	}
+
 	public T min(){
 		if (isEmpty()){
 			throw new RuntimeException();
 		}
-		TreeNode<T> node = root;
-		while(node.left != null){
-			node = node.left;
-		}
-		return node.key;
+		return min(root);
 	}
 	
-	private TreeNode<T> min(TreeNode<T> node){
+	private T min(TreeNode<T> node) {
+		if (node.left == null){
+			return node.key;
+		}
+		
+		return min(node.left);
+	}
+
+	private TreeNode<T> minSubtreeNode(TreeNode<T> node){
 		while(node.left != null){
 			node = node.left;
 		}
 		return node;
 	}
 
+	
 	public T max(){
 		if (isEmpty()){
 			throw new RuntimeException();
 		}
-		TreeNode<T> node = root;
-		while(node.right != null){
-			node = node.right;
-		}
-		return node.key;
+		return max(root);
 	}
 	
-	private TreeNode<T> max(TreeNode<T> node){
+	private T max(TreeNode<T> node) {
+		if (node.right == null){
+			return node.key;
+		}
+		return max(node.right);
+	}
+
+	private TreeNode<T> maxSubtreeNode(TreeNode<T> node){
 		while(node.right != null){
 			node = node.right;
 		}
 		return node;
 		
 	}
-	
-	
 	
 	public void preOrderTraversal() {
 		preOrderTraversal(root);
@@ -188,7 +191,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 				if (node != null){
 					nextLevel.add(node.left);
 					nextLevel.add(node.right);
-					System.out.print(node.key + " ");
+					System.out.print(node.key + ":" + node.size + " ");
 				}
 			}
 			System.out.println();
@@ -211,7 +214,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 				current = current.right;
 			} else {
 				if (current.left != null){
-					return max(current.left).key;
+					return maxSubtreeNode(current.left).key;
 				} else {
 					if (predecessor != null){
 						return predecessor.key;
@@ -224,6 +227,8 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		throw new RuntimeException("No such element");
 	}
 	
+	
+	
 	public T successor(T t){
 		TreeNode<T> current = root;
 		TreeNode<T> successor = null;
@@ -235,7 +240,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 				current = current.right;
 			} else {
 				if (current.right != null){
-					return min(current.right).key;
+					return minSubtreeNode(current.right).key;
 				} else{
 					if(successor != null){
 						return successor.key;
@@ -248,7 +253,122 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		throw new RuntimeException("No such element");
 	}
 	
+	public int size(){
+		return size(root);
+	}
 	
+	
+	private int size(TreeNode<T> node) {
+		if (node == null){
+			return 0;
+		}
+		return size(node.left) + size(node.right) + 1;
+	}
+
+	public int height(){
+		return height(root);
+	}
+	
+	
+	private int height(TreeNode<T> node) {
+		if (node == null){
+			return 0;
+		}
+		return 1 + Math.max(height(node.left), height(node.right));
+	}
+
+	public int leafNodes(){
+		return leafNodes(root);
+	}
+	private int leafNodes(TreeNode<T> node) {
+		if (node == null){
+			return 0;
+		}
+		
+		if (node.left == null && node.right == null){
+			return 1;
+		}
+		
+		return leafNodes(node.left) + leafNodes(node.right);
+	}
+
+	
+	public int width(){
+		int height = height();
+		int max  = 0;
+		for (int i = 1; i <= height; i++){
+			int width = width(root, i);
+			if (width > max){
+				max = width;
+			}
+		}
+		return max;
+	}
+	
+	private int width(TreeNode<T> node, int i) {
+		if (node  == null){
+			return 0;
+		}
+		if (i == 1){
+			return 1;
+		}
+		
+		return width(node.left, i - 1) + width(node.right, i - 1);
+	}
+
+	 public int diameter(){
+		 return diameter(root);
+	 }
+	
+	private int diameter(TreeNode<T> node) {
+		if (node == null){
+			return 0;
+		}
+		
+		int crossRoot = height(node.left) + height(node.right) + 1;
+		int leftSubtree = diameter(node.left);
+		int rightSubtree = diameter(node.right);
+		return Math.max(crossRoot, Math.max(leftSubtree, rightSubtree));
+	}
+	
+	
+	public T select(int rank){
+		return select(root, rank);
+	}
+	
+	private T select(TreeNode<T> node, int rank) {
+		if (node == null){
+			throw new RuntimeException("Cannot find element");
+		}
+		int currentRank = size(node.left) + 1;
+		if (rank < currentRank){
+			return select(node.left, rank);
+		} else if (rank > currentRank){
+			return select(node.right, rank - currentRank);
+		} else {
+			return node.key;
+		}
+	}
+
+	public int rank(T t){
+		return rank(root, t);
+	}
+
+	private int rank(TreeNode<T> node, T t) {
+		if (node == null){
+			throw new RuntimeException("Cannot find element");
+		}
+		
+		if (t.compareTo(node.key) < 0){
+			return rank(node.left, t);
+		} else if (t.compareTo(node.key) > 0){
+			return size(node.left) + rank(node.right, t) + 1;
+		} else {
+    		return size(node.left) + 1;
+		}
+	}
+	
+
 	public static void main(String[] args) {
 		BinarySearchTree<Integer> tree = new BinarySearchTree<>();
 		tree.insert(5);
@@ -258,21 +378,54 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		tree.insert(9);
 		tree.insert(1);
 		tree.insert(6);
-		System.out.println(tree.max());
-		tree.preOrderTraversal();
-		tree.postOrderTraversal();
-		tree.inOrderTraversal();
-		tree.levelOrderTraversal();
+		tree.insert(2);
+		tree.insert(8);
+		tree.delete(3);
 		tree.levelOrderTraversal2();
-//		System.out.println(tree.search(5));
-//		System.out.println(tree.predecessor(3));
-//		System.out.println(tree.successor(1));
-		tree.deleteRecursive(5);
-		tree.levelOrderTraversal2();
-	
+		System.out.println(tree.rank(1));
+		System.out.println(tree.select(7));
 	}
-
-	// too complicated
+	
+//	public boolean search(T t){
+//		TreeNode<T> node = root;
+//		while(node != null){
+//			if (t.compareTo(node.key) == 0){
+//				return true;
+//			} else if (t.compareTo(node.key) < 0){
+//				node = node.left;
+//			} else {
+//				node = node.right;
+//			}
+//		}
+//		return false;
+//	}
+//	
+//	public void insert(T t){
+//		TreeNode<T> newNode = new TreeNode<>(t);
+//		if (isEmpty()){
+//			root = newNode;
+//			return;
+//		}
+//		
+//		TreeNode<T> p = null;
+//		TreeNode<T> node = root;
+//		while(node != null){ 
+//			p = node;
+//			if (t.compareTo(node.key) <= 0){
+//				node = node.left;
+//			} else if (t.compareTo(node.key) > 0){
+//				node = node.right;
+//			} 
+//		}
+//		if (t.compareTo(p.key) <= 0){
+//			p.left = newNode;
+//		} else {
+//			p.right = newNode;
+//		}
+//	}
+//	
+//
+//	// too complicated
 //	public void delete(T t){
 //		if (isEmpty()){
 //			throw new RuntimeException("Empty Tree");
@@ -341,6 +494,27 @@ public class BinarySearchTree<T extends Comparable<T>> {
 //		}
 //		throw new RuntimeException("Cannot find this element");
 //		
+//	}
+//	public T min(){
+//		if (isEmpty()){
+//			throw new RuntimeException();
+//		}
+//		TreeNode<T> node = root;
+//		while(node.left != null){
+//			node = node.left;
+//		}
+//		return node.key;
+//	}
+//	
+//	public T max(){
+//		if (isEmpty()){
+//			throw new RuntimeException();
+//		}
+//		TreeNode<T> node = root;
+//		while(node.right != null){
+//			node = node.right;
+//		}
+//		return node.key;
 //	}
 
 }
--- src/algorithm/datastructure/TreeNode.java
+++ src/algorithm/datastructure/TreeNode.java
@@ -5,10 +5,12 @@ public class TreeNode<T> {
 	public T key;
 	public TreeNode<T> left;
 	public TreeNode<T> right;
+	public int size;
 	
 	public TreeNode(T t){
 		this.key = t;
 		left = null;
 		right = null;
+		size = 1;
 	}
 }
